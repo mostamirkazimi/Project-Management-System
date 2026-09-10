1f2014f48833906255f3aff186eb6ef2108b781f
Mostamir <sayedmostamirkazimi.smk@gmail.com>
2026-09-10 17:27:38 +0430

    fix: configure server for deployment

--- back_nestjs/src/main.ts
+++ back_nestjs/src/main.ts
@@ -46,9 +46,9 @@ app.useGlobalPipes(
   );
 
 
-  await app.listen(
-    process.env.PORT ?? 3000,
-  );
+  const port = process.env.PORT ?? 3000;
+
+    await app.listen(port, '0.0.0.0');
 }
 
 bootstrap();
